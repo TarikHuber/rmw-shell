2dc733ffe5ef11611cdc2973194ae50c5e862564
Tarik Huber <huber.tarik@gmail.com>
2017-11-14 13:43:05 +0100

    Changed test

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,131 +1,220 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { ResponsiveAppBar } from 'material-ui-responsive-drawer';
-import { Helmet } from 'react-helmet';
-import IconButton from 'material-ui/IconButton';
-import FontIcon from 'material-ui/FontIcon';
-import muiThemeable from 'material-ui/styles/muiThemeable';
-import { BodyContainer } from 'material-ui-responsive-drawer';
-import LinearProgress from 'material-ui/LinearProgress';
-import { injectIntl } from 'react-intl';
-import { deepOrange500, darkWhite } from 'material-ui/styles/colors';
-import { withRouter } from 'react-router-dom'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import FlatButton from 'material-ui/FlatButton'
+import { injectIntl, intlShape } from 'react-intl'
+import { GitHubIcon } from 'rmw-shell/lib/components/Icons'
+import { Activity } from 'rmw-core'
+import muiThemeable from 'material-ui/styles/muiThemeable'
+import { Line, Bar, Doughnut } from 'react-chartjs-2'
+import { withFirebase } from 'firekit-provider'
+import CountUp from 'react-countup'
+import FontIcon from 'material-ui/FontIcon'
 
-export class Test extends Component {
+const currentYear = new Date().getFullYear()
+const daysPath = `/user_registrations_per_day/${currentYear}/${new Date().toISOString().slice(5, 7)}`
+const monthsPath = `/user_registrations_per_month/${currentYear}`
+const providerPath = `/provider_count`
 
-    getIconElementLeft = () => {
+class Dashboard extends Component {
+  componentDidMount() {
+    const { watchPath } = this.props
 
-        const { onBackClick } = this.props;
+    watchPath(daysPath)
+    watchPath(monthsPath)
+    watchPath(providerPath)
+    watchPath('users_count')
+  }
 
-        if (onBackClick) {
-            return <IconButton>
-                <FontIcon className="material-icons" >chevron_left</FontIcon>
-            </IconButton>
-        } else {
-            return undefined;
-        }
+  render() {
+    const { muiTheme, intl, days, months, providers, usersCount } = this.props
+
+    let daysLabels = []
+    let daysData = []
+
+    if (days) {
+      Object.keys(days).sort().map(key => {
+        daysLabels.push(key)
+        daysData.push(days[key])
+        return key
+      })
     }
 
-    render() {
-
-        const {
-      muiTheme,
-            title,
-            children,
-            onBackClick,
-            history,
-            intl,
-            isConnected,
-            isLoading,
-            dispatch,
-            containerStyle,
-            pageTitle,
-            height,
-            staticContext,
-            valueLink,
-            customDrawerWidth,
-            ...rest
-    } = this.props;
-
-        const drawerWidth = customDrawerWidth ? customDrawerWidth : 256;
-
-        const bodyContainerStyle = {
-            backgroundColor: muiTheme.palette.canvasColor,
-            top: 64,
-            bottom: 0,
-            overflow: 'auto',
-            ...containerStyle
-        };
-
-        let headerTitle = ''
-
-        if (typeof title === 'string' || title instanceof String) {
-            headerTitle = title;
-        } else {
-            headerTitle = pageTitle;
+    const daysComponentData = {
+      labels: daysLabels,
+      datasets: [
+        {
+          label: intl.formatDate(Date.now(), { month: 'long' }),
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: muiTheme.palette.primary1Color,
+          borderColor: muiTheme.palette.primary1Color,
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: muiTheme.palette.accent1Color,
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: muiTheme.palette.primary1Color,
+          pointHoverBorderColor: muiTheme.palette.accent1Color,
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: daysData
         }
+      ]
+    }
 
-        return (
-            <div style={{ backgroundColor: muiTheme.palette.canvasColor, height: '100%' }}>
-                <Helmet>
-                    <meta name="theme-color" content={muiTheme.palette.primary1Color} />
-                    <meta name="apple-mobile-web-app-status-bar-style" content={muiTheme.palette.primary1Color} />
-                    <meta name="msapplication-navbutton-color" content={muiTheme.palette.primary1Color} />
-                    <title>{headerTitle}</title>
-                </Helmet>
-                <ResponsiveAppBar
-                    width={drawerWidth}
-                    title={title}
-                    showMenuIconButton={onBackClick !== undefined ? true : undefined}
-                    onLeftIconButtonTouchTap={onBackClick}
-                    iconElementLeft={this.getIconElementLeft()}
-                    {...rest}
-                />
-                {!isConnected &&
-                    <div
-                        id="offline-inicator"
-                        style={{
-                            zIndex: 9999,
-                            position: 'fixed',
-                            top: 0,
-                            height: 12,
-                            backgroundColor: deepOrange500,
-                            color: darkWhite,
-                            width: '100%',
-                            fontSize: 12,
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center'
+    let monthsLabels = []
+    let monthsData = []
 
-                        }} >
-                        <span>
-                            {intl.formatMessage({ id: 'no_connection' })}
-                        </span>
-                    </div>
-                }
+    if (months) {
+      Object.keys(months).sort().map(key => {
+        let date = new Date(`${currentYear}-${key}-1`)
+        monthsLabels.push(intl.formatDate(date, { month: 'long' }))
 
-                {isLoading &&
-                    <LinearProgress mode="indeterminate" color={muiTheme.palette.accent1Color} style={{ zIndex: 9998, position: 'fixed', top: 0, height: height ? height : 5 }} />
-                }
+        monthsData.push(months[key])
+        return key
+      })
+    }
 
-                <BodyContainer width={drawerWidth} id="bodyContainer" ref="bodyContainer" withRef style={bodyContainerStyle} >
-                    {children}
-                </BodyContainer>
+    const monthsComponentData = {
+      labels: monthsLabels,
+      datasets: [
+        {
+          label: intl.formatMessage({ id: 'user_registrationg_graph_label' }),
+          fill: false,
+          maintainAspectRatio: true,
+          lineTension: 0.1,
+          backgroundColor: muiTheme.palette.primary1Color,
+          borderColor: muiTheme.palette.primary1Color,
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: muiTheme.palette.accent1Color,
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: muiTheme.palette.primary1Color,
+          pointHoverBorderColor: muiTheme.palette.accent1Color,
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: monthsData
+        }
+      ]
+    }
+
+    let providersData = []
+    let providersLabels = []
+    let providersBackgrounColors = []
+
+    if (providers) {
+      Object.keys(providers).sort().map((key) => {
+        providersLabels.push(intl.formatMessage({ id: key }))
+        providersBackgrounColors.push(intl.formatMessage({ id: `${key}_color` }))
+        providersData.push(providers[key])
+        return key
+      })
+    }
+
+    const providersComponentData = {
+      labels: providersLabels,
+      datasets: [{
+        data: providersData,
+        backgroundColor: providersBackgrounColors,
+        hoverBackgroundColor: providersBackgrounColors
+      }]
+    }
+
+    return (
+      <Activity
+        iconElementRight={
+          <FlatButton
+            style={{ marginTop: 4 }}
+            href='https://github.com/TarikHuber/react-most-wanted'
+            target='_blank'
+            rel='noopener'
+            secondary
+            icon={<GitHubIcon />}
+          />
+        }
+        title={intl.formatMessage({ id: 'dashboard' })}>
+
+        <div style={{ margin: 5, display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center' }}>
+          <div style={{ flexGrow: 1, flexShrink: 1, maxWidth: 600 }}>
+            <Line
+              options={{
+                maintainAspectRatio: true
+              }}
+              data={monthsComponentData}
+            />
+          </div>
+
+          <div style={{ flexGrow: 1, flexShrink: 1, maxWidth: 600 }}>
+            <Bar
+              options={{
+                maintainAspectRatio: true
+              }}
+              data={daysComponentData}
+            />
+          </div>
+
+        </div>
+
+        <br />
+        <div style={{ margin: 5, display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center' }}>
+
+          <div style={{ flexGrow: 1, flexShrink: 1, maxWidth: 600 }}>
+            <Doughnut
+              data={providersComponentData}
+            />
+          </div>
+
+          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', margin: 30 }}>
+            <CountUp
+              style={{
+                fontSize: 100,
+                color: muiTheme.palette.primary1Color,
+                fontFamily: muiTheme.fontFamily
+              }}
+              start={0}
+              end={usersCount}
+            />
+            <div>
+              <FontIcon
+                color={muiTheme.palette.accent1Color}
+                className='material-icons'
+                style={{ fontSize: 70, marginLeft: 16 }}>
+                group
+              </FontIcon>
             </div>
-        );
 
-    }
+          </div>
+        </div>
+
+      </Activity>
+    )
+  }
+}
+
+Dashboard.propTypes = {
+  intl: intlShape.isRequired
 }
 
 const mapStateToProps = (state) => {
-    const { connection, intl } = state;
+  const { paths } = state
 
-    return {
-        isConnected: connection ? connection.isConnected : false,
-        intl
-    };
-};
+  return {
+    days: paths[daysPath],
+    months: paths[monthsPath],
+    providers: paths[providerPath],
+    usersCount: paths['users_count'] ? paths['users_count'] : 0
+  }
+}
 
 export default connect(
-    mapStateToProps,
-)(injectIntl(muiThemeable()(withRouter(Test))));
+  mapStateToProps
+)(injectIntl(muiThemeable()(withFirebase(Dashboard))))
