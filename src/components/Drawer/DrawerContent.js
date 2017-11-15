be545a4529ab9adaa395d57938aee003df5b71aa
Tarik Huber <huber.tarik@gmail.com>
2017-11-15 10:13:04 +0100

    Refactored drawer content component

--- src/components/Drawer/DrawerContent.js
+++ src/components/Drawer/DrawerContent.js
@@ -1,32 +1,19 @@
+import FontIcon from 'material-ui/FontIcon'
 import React from 'react'
 import muiThemeable from 'material-ui/styles/muiThemeable'
+import withAppConfigs from '../../withAppConfigs'
 import { SelectableMenuList } from 'material-ui-selectable-menu-list'
-import FontIcon from 'material-ui/FontIcon'
-import Toggle from 'material-ui/Toggle'
 import { injectIntl } from 'react-intl'
 import { withRouter } from 'react-router-dom'
-import withAppConfigs from '../../withAppConfigs'
 
 export const DrawerContent = (props, context) => {
   const {
-    responsiveDrawer,
-    setResponsive,
-    theme,
-    locale,
-    updateTheme,
-    updateLocale,
-    intl,
-    muiTheme,
-    auth,
-    dialogs,
-    match,
-    messaging,
-    getMenuItems,
     appConfig,
-    isGranted
+    dialogs,
+    intl,
+    match,
+    messaging
   } = props
-
-  const isAuthorised = auth.isAuthorised
 
   const handleChange = (event, index) => {
     const { history, responsiveDrawer, setDrawerOpen } = props
@@ -40,40 +27,17 @@ export const DrawerContent = (props, context) => {
     }
   }
 
-  const themeItems = appConfig.themes.map((t) => {
-    return {
-      value: undefined,
-      visible: true,
-      primaryText: intl.formatMessage({ id: t.id }),
-      onClick: () => { updateTheme(t.id) },
-      rightIcon: <FontIcon
-        className='material-icons'
-        color={t.id === theme ? muiTheme.palette.primary1Color : undefined}>
-        style
-      </FontIcon>
-    }
-  })
-
-  const localeItems = appConfig.locales.map((l) => {
-    return {
-      value: undefined,
-      visible: true,
-      primaryText: intl.formatMessage({ id: l.locale }),
-      onClick: () => { updateLocale(l.locale) },
-      rightIcon: <FontIcon
-        className='material-icons'
-        color={l.locale === locale ? muiTheme.palette.primary1Color : undefined}>
-        language
-      </FontIcon>
-    }
-  })
-
   const menuItems = appConfig.getMenuItems(props)
 
   const handleSignOut = () => {
     const { userLogout, setDialogIsOpen, appConfig } = props
 
+    console.log('props', props)
+    console.log('appConfig', appConfig)
+
     appConfig.firebaseLoad().then(({ firebaseApp }) => {
+      console.log(firebaseApp)
+
       this.firebaseApp = firebaseApp
 
       firebaseApp.database().ref(`users/${firebaseApp.auth().currentUser.uid}/connections`).remove()
--- src/config.js
+++ src/config.js
@@ -5,6 +5,22 @@ import locales from './locales'
 import { themes } from './themes'
 
 const config = {
+  firebase_config: {
+    apiKey: 'AIzaSyBQAmNJ2DbRyw8PqdmNWlePYtMP0hUcjpY',
+    authDomain: 'react-most-wanted-3b1b2.firebaseapp.com',
+    databaseURL: 'https://react-most-wanted-3b1b2.firebaseio.com',
+    projectId: 'react-most-wanted-3b1b2',
+    storageBucket: 'react-most-wanted-3b1b2.appspot.com',
+    messagingSenderId: '258373383650'
+  },
+  firebase_config_dev: {
+    apiKey: 'AIzaSyB31cMH9nJnERC1WCWA7lQHnY08voLs-Z0',
+    authDomain: 'react-most-wanted-dev.firebaseapp.com',
+    databaseURL: 'https://react-most-wanted-dev.firebaseio.com',
+    projectId: 'react-most-wanted-dev',
+    storageBucket: 'react-most-wanted-dev.appspot.com',
+    messagingSenderId: '70650394824'
+  },
   firebase_providers: [
     'google.com',
     'facebook.com',
@@ -23,7 +39,8 @@ const config = {
   getMenuItems: getMenuItems,
   locales: locales,
   routes: [],
-  themes: themes
+  themes: themes,
+  firebaseLoad: () => import('./firebase')
 }
 
 export default config
