5c81a66c2d239daf666ff37a02f3db89f412ca84
Tarik Huber <huber.tarik@gmail.com>
2018-02-15 08:37:04 +0100

    Removed css from index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ export { getGeolocation } from './utils/googleMaps'
 export { setPersistentValue } from './store/persistentValues/actions'
 export { setSimpleValue } from './store/simpleValues/actions'
 
-import './index.css'
+//import './index.css'
 
 export default App
