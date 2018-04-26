ac6232c47d6ea2c1eba1df041f24bafc73dc9032
Tarik Huber <huber.tarik@gmail.com>
2018-04-26 11:43:11 +0200

    Migrated PageNotFound

--- demo/src/containers/About/About.js
+++ demo/src/containers/About/About.js
@@ -1,33 +1,12 @@
 import React, { Component } from 'react'
 import { injectIntl, intlShape } from 'react-intl'
-import { connect } from 'react-redux'
 import Activity from '../../../../src/components/Activity'
-import DrawerActions from '../../../../src/store/drawer/actions'
-import Switch from 'material-ui/Switch'
-import { FormGroup, FormControlLabel } from 'material-ui/Form'
 
 class About extends Component {
   render () {
-    const { drawer, setDrawerOpen, setDrawerMobileOpen, setDrawerUseMinified } = this.props
-
     return (
       <Activity >
-
-        <Switch
-          checked={drawer.open}
-          onChange={() => setDrawerOpen(!drawer.open)}
-        />
-        <br />
-        <Switch
-          checked={drawer.mobileOpen}
-          onChange={() => setDrawerMobileOpen(!drawer.mobileOpen)}
-        />
-        <br />
-        <Switch
-          checked={drawer.useMinified}
-          onChange={() => setDrawerUseMinified(!drawer.useMinified)}
-        />
-
+        TEST
       </Activity>
     )
   }
@@ -37,12 +16,4 @@ About.propTypes = {
   intl: intlShape.isRequired
 }
 
-const mapStateToProps = (state) => {
-  const { drawer } = state
-
-  return {
-    drawer
-  }
-}
-
-export default connect(mapStateToProps, { ...DrawerActions })(injectIntl(About))
+export default injectIntl(About)
--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,4 +1,4 @@
-import ActionHome from '@material-ui/icons/Home'
+import Icon from 'material-ui/Icon'
 import Button from 'material-ui/Button'
 import Paper from 'material-ui/Paper'
 import React from 'react'
@@ -6,23 +6,28 @@ import Typography from 'material-ui/Typography'
 import { withStyles } from 'material-ui/styles'
 import withAppConfigs from '../../withAppConfigs'
 import { injectIntl } from 'react-intl'
+import Activity from '../../components/Activity'
 
 const styles = theme => ({
   icon: {
     width: 192,
-    height: 192
+    height: 192,
+    color: theme.palette.secondary.main
   },
   container: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    margin: 5
+    height: '100%'
   },
   paper: {
-    height: '100%',
-    margin: 0,
-    padding: 1
+    backgroundColor: theme.palette.background.default,
+    height: '100vh',
+    margin: 0
+  },
+  button: {
+    marginTop: 20
   }
 
 })
@@ -31,16 +36,23 @@ const PageNotFound = ({ muiTheme, intl, appConfig, classes }) => {
   const AppIcon = appConfig.appIcon
 
   return (
+    <Activity>
+      <Paper className={classes.paper}>
+        <div className={classes.container}>
+          <AppIcon className={classes.icon} />
+          <Typography variant='display1' >
+            {intl.formatMessage({ id: 'warning_404_message' })}
+          </Typography>
+          <Typography variant='subheading' >
+            {intl.formatMessage({ id: 'warning_404_description' })}
+          </Typography>
+          <Button variant='fab' color='secondary' aria-label='home' href='/' className={classes.button} >
+            <Icon>home</Icon>
+          </Button>
+        </div>
 
-    <Typography className={classes.paper}>
-      <div className={classes.container}>
-        <AppIcon className={classes.icon} />
-        <h3>{intl.formatMessage({ id: 'warning_404_message' })}</h3>
-        <p>{intl.formatMessage({ id: 'warning_404_description' })}</p>
-      </div>
-
-    </Typography>
-
+      </Paper>
+    </Activity>
   )
 }
 
