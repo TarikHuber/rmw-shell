982ef9763d68a34affe5b4fe641f690c1d1e247b
TarikHuber <huber.tarik@gmail.com>
2018-07-24 10:25:45 +0200

    Refactored AltIconAatar component

--- src/components/AltIconAvatar/AltIconAvatar.js
+++ src/components/AltIconAvatar/AltIconAvatar.js
@@ -3,21 +3,21 @@ import Avatar from '@material-ui/core/Avatar'
 import Icon from '@material-ui/core/Icon'
 
 const AltIconAvatar = (props) => {
-  const { src, iconName, icon } = props
+  const { src, iconName, icon, ...rest } = props
 
   if (src) {
-    return <Avatar src={src} {...props} />
+    return <Avatar src={src} {...rest} />
   }
 
   if (icon) {
-    return <Avatar {...props}> {icon} </Avatar>
+    return <Avatar {...rest}> {icon} </Avatar>
   }
 
   if (iconName) {
-    return <Avatar {...props}><Icon > {iconName}</Icon></Avatar>
+    return <Avatar {...rest}><Icon > {iconName}</Icon></Avatar>
   }
 
-  return <Avatar {...props} />
+  return <Avatar {...rest} />
 }
 
 export default AltIconAvatar
