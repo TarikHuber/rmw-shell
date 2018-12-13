b0c7b952c8e63bfc375b550df5699106a012d335
Tarik Huber <huber.tarik@gmail.com>
2018-12-13 11:53:11 +0100

    Added FallbackComponent to restricted route

--- src/containers/RestrictedRoute/RestrictedRoute.js
+++ src/containers/RestrictedRoute/RestrictedRoute.js
@@ -3,12 +3,20 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router'
 
-export const RestrictedRoute = ({ type, isAuthorised, component: Component, ...rest }) => (
+export const RestrictedRoute = ({
+  type,
+  isAuthorised,
+  component: Component,
+  fallbackComponent: FallbackComponent = false,
+  ...rest
+}) => (
   <Route
     {...rest}
     render={props => {
       if ((isAuthorised && type === 'private') || (!isAuthorised && type === 'public')) {
         return <Component {...props} />
+      } else if (FallbackComponent) {
+        return <FallbackComponent {...props} />
       } else {
         return (
           <Redirect
