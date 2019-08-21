5a7728b1e9ec2a81d3f00608336ccbe4611a3b90
Tarik Huber <tarik.huber@ics.local>
2019-08-21 14:12:19 +0200

    Fixed bug in FreeAutocomplete field

--- src/components/ReduxFormFields/FreeAutocomplete.js
+++ src/components/ReduxFormFields/FreeAutocomplete.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import { FreeAutocomplete } from 'muishift'
+import Muishift, { FreeAutocomplete } from 'muishift'
+import Downshift from 'downshift'
 
-const FormFreeAutocomplete = ({ label, input, meta: { touched, invalid, error }, ...custom }) => (
-  <FreeAutocomplete
-    label={label}
-    placeholder={label}
-    error={touched && invalid}
-    helperText={touched && error}
-    {...input}
-    {...custom}
-  />
-)
+const FormFreeAutocomplete = props => {
+  const {
+    label,
+    input,
+    meta: { touched, invalid, error },
+    ...custom
+  } = props
+
+  return (
+    <FreeAutocomplete
+      label={label}
+      placeholder={label}
+      error={touched && invalid}
+      helperText={touched && error}
+      selectedItem={input.value}
+      {...custom}
+      {...input}
+    />
+  )
+}
 
 export default FormFreeAutocomplete
